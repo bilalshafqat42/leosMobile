4917f0c02ad7a607276aa331c55fdd38db426fd6
bilalshafqat42 <bilalshafqat42@gmail.com>
2023-10-05 14:06:35 +0400

    splash screen second update

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
 import Navigation from './src/navigation';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AuthProvider from './src/navigation/AuthProvider';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    if (Platform.OS === 'android') SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaProvider>
-     <AuthProvider>
-      <Navigation />
+      <AuthProvider>
+        <Navigation />
       </AuthProvider>
     </SafeAreaProvider>
   );
@@ -16,5 +20,4 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
